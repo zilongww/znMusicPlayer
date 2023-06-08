1b8d456f54627cd2f85e1491eda088382e6e0cbc
zi long <zilongcn233@outlook.com>
2023-06-09 03:09:00 +0800

    23-6-9-2

--- znMusicPlayerWUI/znMusicPlayerWUI/App.xaml.cs
+++ znMusicPlayerWUI/znMusicPlayerWUI/App.xaml.cs
@@ -64,7 +64,7 @@ namespace znMusicPlayerWUI
         public static IntPtr AppDesktopLyricWindowHandle;
 
         public static readonly string AppName = "znMusicPlayer";
-        public static readonly string AppVersion = "0.1.99 Preview";
+        public static readonly string AppVersion = "0.2.0 Preview";
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
