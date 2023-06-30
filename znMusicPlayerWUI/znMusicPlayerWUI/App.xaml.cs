0a9f889ae8251fc9a80262f315d2432f09152d8a
zi long <zilongcn233@outlook.com>
2023-07-01 04:07:06 +0800

    更换音乐界面背景颜色

--- znMusicPlayerWUI/znMusicPlayerWUI/App.xaml.cs
+++ znMusicPlayerWUI/znMusicPlayerWUI/App.xaml.cs
@@ -64,7 +64,7 @@ namespace znMusicPlayerWUI
         public static IntPtr AppDesktopLyricWindowHandle;
 
         public static readonly string AppName = "znMusicPlayer";
-        public static readonly string AppVersion = "0.2.1 Preview";
+        public static readonly string AppVersion = "0.2.2 Preview";
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
