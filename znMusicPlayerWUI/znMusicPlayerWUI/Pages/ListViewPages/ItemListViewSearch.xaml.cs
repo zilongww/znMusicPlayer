012a4084418c0e6419cbfbb58544dcdb45031cd4
zi long <zilongcn233@outlook.com>
2024-04-16 04:21:13 +0800

    updata

--- znMusicPlayerWUI/znMusicPlayerWUI/Pages/ListViewPages/ItemListViewSearch.xaml.cs
+++ znMusicPlayerWUI/znMusicPlayerWUI/Pages/ListViewPages/ItemListViewSearch.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
